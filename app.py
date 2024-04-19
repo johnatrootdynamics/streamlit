d81020c6e04254777922a209321db9aef81bd5ad
johnatrootdynamics <john.abraham@root-dynamics.com>
2024-04-19 15:53:39 -0400

    sdf

---
+++ app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+
+
+tab1, tab2 = st.tabs(["Tab 1", "Tab2"])
+tab1.write("this is tab 1")
+tab2.write("this is tab 2")
+
+# You can also use "with" notation:
+with tab1:
+    st.radio("Select one:", [1, 2])
