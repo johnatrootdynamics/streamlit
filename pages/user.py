a79c212e309eecd4b94cac85759b1c96b5bb824c
johnatrootdynamics <john.abraham@root-dynamics.com>
2024-04-21 04:25:00 -0400

    sdf

--- pages/user.py
+++ pages/user.py
@@ -10,7 +10,7 @@ st.markdown(f"You are currently logged with the role of {st.session_state.role}.
 col1,col2,col3 = st.columns(1)
 col1.write('First Name')
 col2.write('Last Name')
-col3.write('Class')
+col3.write('ID')
 
 
 for row in df.itertuples():
@@ -19,8 +19,8 @@ for row in df.itertuples():
     with col2:
         st.write(row.last_name)
     with col3:
-        st.write(row.class)
-        
+        st.write(row.id)
+
 
 
 
