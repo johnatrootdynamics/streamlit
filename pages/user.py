6ab692e35ee9f175d5a7b23b701868ba73957078
johnatrootdynamics <john.abraham@root-dynamics.com>
2024-04-21 03:52:14 -0400

    sssdf

--- pages/user.py
+++ pages/user.py
@@ -7,6 +7,16 @@ conn = st.connection('mysql', type='sql')
 df = conn.query('SELECT * from drivers;', ttl=600)
 st.title("This page is available to all users")
 st.markdown(f"You are currently logged with the role of {st.session_state.role}.")
+col1,col2 = st.columns(2)
+col1.write('First Name')
+col2.write('Last Name')
+
 
 for row in df.itertuples():
-    st.write(f"{row.first_name} {row.last_name}:")
+    with col1:
+        st.write(row.first_name)
+    with col2:
+        st.write(row.last_name)
+
+
+
