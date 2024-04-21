4362b9110c013aa470a46f1cae54142f4bc3ed9f
johnatrootdynamics <john.abraham@root-dynamics.com>
2024-04-21 03:37:55 -0400

    sdf

--- pages/user.py
+++ pages/user.py
@@ -3,6 +3,7 @@ from menu import menu_with_redirect
 
 # Redirect to app.py if not logged in, otherwise show the navigation menu
 menu_with_redirect()
+conn = st.connection('mysql', type='sql')
 df = conn.query('SELECT * from drivers;', ttl=600)
 st.title("This page is available to all users")
 st.markdown(f"You are currently logged with the role of {st.session_state.role}.")
